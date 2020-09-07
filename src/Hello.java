6179b000557785f6f340a4c5cca0e7657fc4cc19
mrjbhl <70877369+mrjbhl@users.noreply.github.com>
2020-09-07 11:38:49 +0200

    Initial commit

---
+++ src/Hello.java
@@ -0,0 +1,9 @@
+public class Hello {
+    // psvm
+    public static void main(String[] args) {
+        //sout
+        System.out.println("Hello World");
+        System.out.println("Look at the above");
+        System.out.println("It WORKS");
+    }
+}
